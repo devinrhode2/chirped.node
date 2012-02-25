d4aa9bfd006247101408ae5ebed26192f0ce5b06
Devin Rhode <devinrhode2@gmail.com>
2012-02-24 21:06:38 -0800

    foo bar, moving styles to main.styl DONT touch main.css (the compiled version) and… yeah

--- server.js
+++ server.js
@@ -4,7 +4,9 @@
  */
 
 var express = require('express')
-  , io = require('socket.io');
+  , io = require('socket.io')
+  , stylus = require('stylus')
+  , nib = require('nib');
 
 var app = module.exports = express.createServer();
 
@@ -16,7 +18,19 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'your secret here' }));
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  function compile(str, path) {
+    return stylus(str)
+      .set('filename', path)
+      .set('compress', true)
+      .use(nib())
+      .import('nib');
+  }
+  
+  app.use(stylus.middleware({
+      src: __dirname + '/public'
+    , compile: compile
+  }));
+  //app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -61,16 +75,25 @@ io.sockets.on('connection', function(socket){
 //Setup NowJS
 var nowjs = require("now");
 var everyone = nowjs.initialize(app);
+
+//pulled from the now npm module directory (node_modules/now/examples/multiroomchat_example)
 nowjs.on('connect', function(){
+  this.now.room = "room 1";
+  nowjs.getGroup(this.now.room).addUser(this.user.clientId);
   console.log("Joined: " + this.now.name);
 });
 nowjs.on('disconnect', function(){
   console.log("Left: " + this.now.name);
 });
-
-//NowJS stuff.
-everyone.now.distributeMessage = function(message){
-  everyone.now.receiveMessage(this.now.name, message);
+everyone.now.changeRoom = function(newRoom){
+  nowjs.getGroup(this.now.room).removeUser(this.user.clientId);
+  nowjs.getGroup(newRoom).addUser(this.user.clientId);
+  this.now.room = newRoom;
+  this.now.receiveMessage("SERVER", "You're now in " + this.now.room);
 };
+everyone.now.distributeMessage = function(message){
+  nowjs.getGroup(this.now.room).now.receiveMessage(this.now.name, message);
+};
+
 
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
